4b813c81fa337a5de32b56b19479e803eb65e843
David <ezehudavid@gmail.com>
2022-11-02 17:14:48 -0700

    my secound commit

--- binary/settings.py
+++ binary/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'binary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'JzkWM5MscMvfLhfvkvlC',
+         'HOST': 'containers-us-west-54.railway.app',
+         'PORT': '6749',
+
     }
 }
 
